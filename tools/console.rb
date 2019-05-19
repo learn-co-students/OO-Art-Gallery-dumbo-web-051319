309596cdc17c4064413447dd45ea2f8bba4dda11
April <fanmeng0417@gmail.com>
2019-05-19 17:05:12 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,20 @@
 require_relative '../config/environment.rb'
 
+tom_a = Artist.new("Tom",10)
+tim_a = Artist.new("Tim",5)
+alice_a = Artist.new("Alice",2)
 
+nyc_art_g = Gallery.new("NYC Art Museum", "NYC")
+dc_art_g = Gallery.new("DC Art Museum", "DC")
+
+love_p = Painting.new("Love", 10000, tom_a, nyc_art_g)
+kind_p = Painting.new("Kind", 20000, tom_a, dc_art_g)
+happy_p = Painting.new("Happy", 30000, tom_a, dc_art_g)
+
+fly_p = Painting.new("Fly", 500000, tim_a, nyc_art_g)
+
+king_p = Painting.new("King", 15000, alice_a, dc_art_g)
+wind_p = Painting.new("Wind", 20000, alice_a, dc_art_g)
 
 
 binding.pry
