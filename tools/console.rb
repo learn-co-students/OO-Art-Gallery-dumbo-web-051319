dbc9e12e26a6bc9aa7e95c0b1d4ee8f4e4645e92
Your Name <you@example.com>
2019-05-21 23:36:25 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,5 +1,17 @@
 require_relative '../config/environment.rb'
 
+artist1 = Artist.new("bob",3)
+artist2 = Artist.new("john",13)
+artist3 = Artist.new("kev",3)
+
+painting1= Painting.new("Hey",38)
+painting2= Painting.new("Starry Night",3006)
+painting3= Painting.new("Mona Lisa",12000)
+
+gallery1=Gallery.new("MoMA","NYC")
+gallery2=Gallery.new("Barcelona Museum","Barcelona")
+
+
 
 
 
