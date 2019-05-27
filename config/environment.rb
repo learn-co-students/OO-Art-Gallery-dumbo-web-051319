a65d9612496216715492a205a65b2149d8c4af50
L.R.L <DaleLoreNY@gmail.com>
2019-05-26 20:01:38 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,27 @@ require 'pry'
 require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
+
+
+
+#DOMAIN MODEL
+# ______________                 _______________ 
+#|              |               |               |
+#|  ARTIST      |               |   GALLERY     |
+#|______________|               |_______________| 
+#       |                               |
+#       |                               |   
+#       |__________         ____________|
+#                  |       |   
+#                  ^       ^
+#               ________________
+#              |                |
+#              |    PAINTINGS   |
+#              |________________|   
+
+# ARTIST has many GALLERIES through PAINTINGS
+# GALLERY has many ARTISTS through PAINTINGS
+# PAINTINGS belongs to ARTIST and GALLERIES
+
+
+
